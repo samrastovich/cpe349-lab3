82e14fc915e992ece8f65343ff1f7bf93518cd0c
samrastovich <sam.rastovich@gmail.com>
2017-02-13 10:39:00 -0800

    Compiled version of game problem

--- GameProblem.java
+++ GameProblem.java
@@ -1,12 +1,33 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class GameProblem {
+   private static final char EXIT = 'e';
+   private static final char RIGHT = 'r';
+   private static final char LEFT = 'l';
+   private static final char DOWN = 'd';
+   private static final char UP = 'u';
 
    public static void game(int n, int m, int[][] A) {
       int[][] S = new int[n][m];
+      char[][] R = new char[n][m];
       
-      
-      for (int i = 0; i < n; i++) {
-         for (int j = 0; j < m; j++) {
-            
+      for (int i = n - 1; i >= 0; i--) {
+         for (int j = m - 1; j >= 0; j--) {
+            if (i == n - 1 && j == m - 1) {
+               R[i][j] = EXIT;
+               S[i][j] += A[i][j];
+            }
+            else if (j == m - 1) {
+               S[i][j] += (max(S[i + 1][j], 0) + A[i][j]);
+            }
+            else if (i == n -1) {
+               S[i][j] += (max(S[i][j + 1], 0) + A[i][j]);
+            }
+            else {
+               
+            }
          }
       }
    }
